cad8c039593ab377668dc3ceca6d6171e6dacda5
Michał Ganiebny <michalganiebny@rocketmail.com>
2023-01-19 16:34:09 +0100

    Add NetworkManager singleton for making REST API requests, getFollowers func, fix bug in GFButton

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -14,6 +14,18 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            
+            print("Followers.count = \(followers.count)")
+            print(followers)
+            
+            
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
