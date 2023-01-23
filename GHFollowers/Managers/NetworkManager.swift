ffc9f277a08d8236de3ac6c76c6de429700d3bc5
Michał Ganiebny <michalganiebny@rocketmail.com>
2023-01-23 11:47:47 +0100

    Add downloadImage func to GFAvatarImageView and image cache to NetworkManager

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,12 +5,12 @@
 //  Created by Michał Ganiebny on 19/01/2023.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
