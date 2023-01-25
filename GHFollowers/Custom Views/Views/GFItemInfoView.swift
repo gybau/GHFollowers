5a15148d3319c038e4c4110a2a4c7f4612d2f1bb
Michał Ganiebny <michalganiebny@rocketmail.com>
2023-01-25 23:56:38 +0100

    Add favoritesListVC tableView and swipe to delete action

--- GHFollowers/Custom Views/Views/GFItemInfoView.swift
+++ GHFollowers/Custom Views/Views/GFItemInfoView.swift
@@ -48,8 +48,8 @@ class GFItemInfoView: UIView {
             titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             
             countLabel.topAnchor.constraint(equalTo: symbolImageView.bottomAnchor, constant: 4),
-            countLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            countLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+            countLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor)
+            //countLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor)
         ])
     }
     
