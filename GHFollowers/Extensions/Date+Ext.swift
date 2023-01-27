a2a609ba3f3b3e591a1a699767f13d9381e71f36
Michał Ganiebny <michalganiebny@rocketmail.com>
2023-01-27 19:49:57 +0100

    Date+Ext refactor with new Date modifiers

--- GHFollowers/Extensions/Date+Ext.swift
+++ GHFollowers/Extensions/Date+Ext.swift
@@ -10,11 +10,6 @@ import Foundation
 extension Date {
     
     func convertToMonthYearFormat() -> String {
-        let dateFormatter           = DateFormatter()
-        dateFormatter.locale        = Locale(identifier: "en_US")
-        dateFormatter.dateFormat    = "MMMM yyyy"
-        
-        
-        return dateFormatter.string(from: self)
+        return formatted(.dateTime.month().year().locale(Locale(identifier: "en_US")))
     }
 }
